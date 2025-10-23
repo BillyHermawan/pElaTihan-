9b951a8636d2a85fab8eefafc666b14dcebf76b4
BillyHermawan <xaviorzyx@gmail.com>
2025-10-23 10:11:13 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+// 1. Pilih tombol berdasarkan 10-nya
+const tombolsapa = document.getElementById('sapaButton');
+// 2. Tambahkan 'event listener' untuk aksi 'click'
+tombolsapa.addEventListener('click'), Function() {
+ // 3. Jalankan kode ini ketika tombol di klik
+ alert ('Halo! Terima kasih sudah berkunjung');
+})
